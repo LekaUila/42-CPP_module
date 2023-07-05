a4aeea19dd43d5cc96d4894ad5ec92623d81050d
Liam Flandrinck <lflandri@2D1.42angouleme.fr>
2023-07-05 11:25:03 +0200

    correction CPP\ 01/ex00   : not all file expected by the subkect

--- CPP 01/ex00/newZombie.cpp
+++ CPP 01/ex00/newZombie.cpp
@@ -1,38 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lflandri <lflandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/07 11:08:48 by lflandri          #+#    #+#             */
-/*   Updated: 2023/07/05 11:22:21 by lflandri         ###   ########.fr       */
+/*   Created: 2023/07/05 11:20:54 by lflandri          #+#    #+#             */
+/*   Updated: 2023/07/05 11:22:28 by lflandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-
-
-
-
-Zombie::Zombie(std::string name)
-:	name(name)
+Zombie* newZombie( std::string name )
 {
-	std::cout << "Zombie \"" << this->name
-	<< "\" has been summun by the darness."
-	<< std::endl;
-}
-
-Zombie::~Zombie()
-{
-	std::cout << "Zombie \"" << this->name
-	<< "\" has been killed by the sun."
-	<< std::endl;
-}
-
-void Zombie::announce( void )
-{
-	std::cout << name << ": BraiiiiiiinnnzzzZ...."
-	<< std::endl;
+	Zombie *zombzomb = new Zombie(name);
+	return zombzomb;
 }
--- CPP 01/ex00/randomChump.cpp
+++ CPP 01/ex00/randomChump.cpp
@@ -1,38 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lflandri <lflandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/07 11:08:48 by lflandri          #+#    #+#             */
-/*   Updated: 2023/07/05 11:22:21 by lflandri         ###   ########.fr       */
+/*   Created: 2023/07/05 11:20:35 by lflandri          #+#    #+#             */
+/*   Updated: 2023/07/05 11:22:13 by lflandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-
-
-
-
-Zombie::Zombie(std::string name)
-:	name(name)
+void randomChump( std::string name )
 {
-	std::cout << "Zombie \"" << this->name
-	<< "\" has been summun by the darness."
-	<< std::endl;
-}
-
-Zombie::~Zombie()
-{
-	std::cout << "Zombie \"" << this->name
-	<< "\" has been killed by the sun."
-	<< std::endl;
-}
-
-void Zombie::announce( void )
-{
-	std::cout << name << ": BraiiiiiiinnnzzzZ...."
-	<< std::endl;
+	Zombie	zombzomb(name);
+	zombzomb.announce();
 }
